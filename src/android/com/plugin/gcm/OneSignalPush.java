23eae6c028c0b91bd8f782b85bc06593bc22a583
afonso-rocha <131665139+afonso-rocha@users.noreply.github.com>
2023-09-27 15:34:23 +0100

    Update OneSignalPush.java

--- src/android/com/plugin/gcm/OneSignalPush.java
+++ src/android/com/plugin/gcm/OneSignalPush.java
@@ -160,7 +160,7 @@ public class OneSignalPush extends CordovaPlugin {
               googleProjectNumber,
               appId,
               new CordovaNotificationOpenedHandler(notifOpenedCallbackContext),
-              new NotificationService()
+              new CordovaNotificationReceivedHandler(notifReceivedCallbackContext)
       );
 
 

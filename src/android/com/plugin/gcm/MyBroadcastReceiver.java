eeb2043950191fe425af20058e85ad43deaa0d22
vansnas <vanessanassali@gmail.com>
2024-10-09 09:46:38 +0100

    Update MyBroadcastReceiver.java

--- src/android/com/plugin/gcm/MyBroadcastReceiver.java
+++ src/android/com/plugin/gcm/MyBroadcastReceiver.java
@@ -11,10 +11,23 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i(TAG, "onReceive: action=" + intent.getAction());
-        if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) || intent.getAction().equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
+        if (intent == null || context == null) {
+            Log.e(TAG, "Received null context or intent, cannot proceed.");
+            return;
+        }
+
+        String action = intent.getAction();
+        Log.i(TAG, "onReceive: action=" + action);
+
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action) || Intent.ACTION_MY_PACKAGE_REPLACED.equals(action)) {
+            // Intent for starting the foreground service
             Intent serviceIntent = new Intent(context, MyForegroundService.class);
-            context.startForegroundService(serviceIntent);
+            try {
+                context.startForegroundService(serviceIntent);
+                Log.i(TAG, "Foreground service started successfully.");
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to start foreground service", e);
+            }
         }
     }
 }
